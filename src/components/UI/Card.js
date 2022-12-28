83f0e771cc0ae23d9c3c0184abfbbdbfd42cdb86
Zezheng Jiang <Zjian107@syr.edu>
2022-12-27 23:48:43 -0500

    ✨Update: listening to event, passing data from child to parent, working with state

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,5 @@
 import "./Card.css";
 
-// TODO: video 41: A closer look at jsx 0:24
 function Card(props) {
   const classes = "card " + props.className;
   return <div className={classes}>{props.children}</div>;
